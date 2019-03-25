709602d6c53b5aa8b7fce5dfd7c9ef604b156ad0
Ettore_Rizza <ettorerizza@gmail.com>
2019-03-25 14:41:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from setuptools import setup
 
 setup(
     name='ugesco',
-    version='0.1',
+    version='0.3',
     packages=['ugfunctions'],
-    url='https://github.com/ettorerizza/UGESCO',
+    url='https://github.com/ulbstic/ugescovalidator',
     license='MIT',
     author='Ettore Rizza',
     author_email='erizza@ulb.ac.be',
-    description='clustering and disambiguisation of a list of place names'
+    description='clustering and disambiguisation of a list of place names',
+    with open('requirements.txt', 'r') as f:
+    install_reqs=[
+        s for s in [
+            line.strip(' \n') for line in f
+        ] if not s.startswith('#') and s != ''
+    ]
 )
